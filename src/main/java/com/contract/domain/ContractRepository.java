2afff911d5eaf9662187fc5178d1421b83729a47
Jaden <owslove777@name.com>
2022-04-22 17:15:26 +0900

    first Commit

---
+++ src/main/java/com/contract/domain/ContractRepository.java
@@ -0,0 +1,7 @@
+package com.contract.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractRepository extends JpaRepository<Contract, Integer>{
+    
+}
---
+++ src/main/java/com/contract/domain/ContractUpdated.java
@@ -0,0 +1,7 @@
+package com.contract.domain;
+
+import com.contract.domain.ContractReserved;
+
+public class ContractUpdated extends ContractReserved{
+    
+}
